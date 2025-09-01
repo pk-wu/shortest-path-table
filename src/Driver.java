89d793ad4a63155938d63e21a30355fc4273a4e8
pk-wu <pattanapong1997k@gmail.com>
2025-09-01 10:51:46 +0800

    added additional method to parse string

--- src/Driver.java
+++ src/Driver.java
@@ -4,6 +4,15 @@ public class Driver {
     public static void main(String[] args) {
         // initialize the graph as an adjacency matrix
         // either directly as int[][] or with string
+//        int[][] graph = {
+//                {0, 1, 3, 0, 0, 10, 0},
+//                {1, 0, 1, 7, 5, 0, 2},
+//                {3, 1, 0, 9, 3, 0, 0},
+//                {0, 7, 9, 0, 2, 0, 0},
+//                {0, 5, 3, 2, 0, 2, 0},
+//                {10, 0, 0, 1, 2, 0, 0},
+//                {0, 2, 0, 12, 0, 0, 0}
+//        };
         int[][] graph = {
                 {0, 1, 3, 0, 0, 10, 0},
                 {1, 0, 1, 7, 5, 0, 2},
@@ -19,15 +28,22 @@ public class Driver {
         // initialize the graph as an adjacency matrix
         // using string representation, separated by spaces ( ) and commas (,)
         String s =
-                "0 1 3 0 0 10 0," +
-                "1 0 1 7 5 0 2," +
-                "3 1 0 9 3 0 0," +
-                "0 7 9 0 2 0 0," +
-                "0 5 3 2 0 2 0," +
-                "10 0 0 1 2 0 0," +
-                "0 2 0 12 0 0 0";
+                "0 5 3 0 0 7," +
+                "5 0 6 2 4 0," +
+                "3 6 0 3 0 8," +
+                "0 2 3 0 2 0," +
+                "0 4 0 2 0 0," +
+                "7 0 8 0 0 0";
         // call stringToGraph() method to parse it into 2D int[][]
-        int[][] newGraph = stringToGraph(s);
+//        int[][] newGraph = stringToGraph(s);
+        String q = "0\t5\t3\t0\t0\t7\n" +
+                "5\t0\t6\t2\t4\t0\n" +
+                "3\t6\t0\t3\t0\t0\n" +
+                "0\t2\t3\t0\t2\t0\n" +
+                "0\t4\t0\t2\t0\t0\n" +
+                "7\t0\t8\t0\t0\t0";
+        int[][] newGraph = sheetsStringToGraph(q);
+
 
 //        int[][] graph = {
 //                {0, 100, 0, 160, 0},
@@ -62,7 +78,7 @@ public class Driver {
         // Input the adjacency matrix, graph (2D int[][])
         // + start node (char)
         // + end node (char)
-        ShortestTable shortestTable = new ShortestTable(graph,'A','F');
+        ShortestTable shortestTable = new ShortestTable(newGraph,'A','D');
     }
 
 
@@ -79,6 +95,19 @@ public class Driver {
         for (int i = 0; i < rows.length; i++) {
             // split each row into another String[] array
             String[] currentRow = rows[i].split(" ");
+            for (int j = 0; j < rows.length; j++) {
+                graph[i][j] = Integer.parseInt(currentRow[j]);
+            }
+        }
+        return graph;
+    }
+
+    public static int[][] sheetsStringToGraph(String s) {
+        String[] rows = s.split("\n");
+        int[][] graph = new int[rows.length][rows.length];
+        for (int i = 0; i < rows.length; i++) {
+            // split each row into another String[] array
+            String[] currentRow = rows[i].split("\t");
             for (int j = 0; j < rows.length; j++) {
                 graph[i][j] = Integer.parseInt(currentRow[j]);
             }
